e90b9a68bf69a1172830f0ddcd76c5501b7a3bd8
keviboi08 <moldovank2008@gmail.com>
2025-12-19 21:42:27 +0100

    Task2g
    made PointGiverSpawner coroutine (10 orbs, random timer)

---
+++ Assets/Scripts/PointGiverSpawner.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic; // <-- Added this line (missing!)
+using UnityEngine;
+
+public class PointGiverSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject energyOrbPrefab;
+    [SerializeField] float minTime = 1.5f;
+    [SerializeField] float maxTime = 4f;
+    int spawned = 0;
+
+    void Start()
+    {
+        StartCoroutine(SpawnOrbs());
+    }
+
+    IEnumerator SpawnOrbs()
+    {
+        while (spawned < 10)
+        {
+            yield return new WaitForSeconds(Random.Range(minTime, maxTime));
+            Instantiate(energyOrbPrefab, transform.position, Quaternion.identity);
+            spawned++;
+            if (spawned >= 10)
+            {
+                // FindObjectOfType<LevelManager>().AllOrbsSpawned(); // Comment this for now (LevelManager not created yet)
+            }
+        }
+    }
+}
